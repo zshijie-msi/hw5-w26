b2cfd17902472ba90d46df62fecd59095b69d353
Shijie Zhang <zshijie@umich.edu>
2026-04-10 16:11:37 -0400

    Add files via upload

---
+++ js/video.js
@@ -0,0 +1,61 @@
+var video;
+
+window.addEventListener("load", function() {
+	console.log("Good job opening the window");
+	video = document.querySelector("#player1");
+	video.autoplay = false;
+	video.loop = false;
+});
+
+document.querySelector("#play").addEventListener("click", function() {
+	video.play();
+	document.querySelector("#volume").innerHTML = video.volume * 100;
+});
+
+document.querySelector("#pause").addEventListener("click", function() {
+	video.pause();
+});
+
+document.querySelector("#slower").addEventListener("click", function() {
+	video.playbackRate = video.playbackRate * 0.9;
+	console.log(video.playbackRate);
+});
+
+document.querySelector("#faster").addEventListener("click", function() {
+	video.playbackRate = video.playbackRate / 0.9;
+	console.log(video.playbackRate);
+});
+
+document.querySelector("#skip").addEventListener("click", function() {
+	if (video.currentTime + 10 >= video.duration) {
+		video.currentTime = 0;
+	}
+	else {
+		video.currentTime = video.currentTime + 10;
+	}
+	console.log(video.currentTime);
+});
+
+document.querySelector("#mute").addEventListener("click", function() {
+	if (video.muted == false) {
+		video.muted = true;
+		document.querySelector("#mute").innerHTML = "Unmute";
+	}
+	else {
+		video.muted = false;
+		document.querySelector("#mute").innerHTML = "Mute";
+	}
+});
+
+document.querySelector("#slider").addEventListener("change", function() {
+	video.volume = document.querySelector("#slider").value / 100;
+	document.querySelector("#volume").innerHTML = document.querySelector("#slider").value;
+});
+
+document.querySelector("#vintage").addEventListener("click", function() {
+	video.classList.add("oldSchool");
+});
+
+document.querySelector("#original").addEventListener("click", function() {
+	video.classList.remove("oldSchool");
+});
